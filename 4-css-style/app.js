a52edafbf6486ed422b2ee73d5ba6287bf8833b6
fdemirag <f2000fatih@hotmail.com>
2025-03-24 16:45:43 +0300

    .

---
+++ 4-css-style/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data(){
+        return{
+              box1Class:false,
+              border:false,
+              price :0
+        }
+       },
+       methods:{
+          changeColor(){
+
+          }
+       },
+       computed:{
+        
+       }
+
+       
+  
+}).mount("#app")
